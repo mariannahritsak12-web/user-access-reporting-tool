31ad95cb5558525bad34d2c2c1a3fa59a22bc074
Marianna <mariannahritsak12@gmail.com>
2026-03-16 22:06:21 +0100

    Refactored the code into class based structure. Added methods to generate report and read log file. Added main function to run the report. Added filtering of events based on time range. Improved error handling when reading log file.

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+# User Access Reporting Tool
+# This script read a log file with all users login and logout events, and generates a report of which users were logged in on each machine in a given time
+
+import csv
+import datetime
+
+# Represents a single login or logout event
 class Event:
   def __init__(self, event_date, event_type, machine_name, user):
     self.date = event_date
@@ -5,39 +12,68 @@ class Event:
     self.machine = machine_name
     self.user = user
 
+class UserAccessReport:
+  def __init__(self):
+    pass
 
-def get_event_date(event):
-  return event.date
+  # Reads the log file and returns a list of all event objects
+  def read_log_file(self, filename):
+    try:
+      with open(filename, "r") as logs:
+        events = csv.DictReader(logs, skipinitialspace=True)
 
-def current_users(events):
-  events.sort(key=get_event_date)
-  machines = {}
-  for event in events:
-    if event.machine not in machines:
-      machines[event.machine] = set()
-    if event.type == "login":
-      machines[event.machine].add(event.user)
-    elif event.type == "logout":
-      if event.user in machines[event.machine]:
-        machines[event.machine].remove(event.user)
-  return machines
+        return [
+          Event(
+            datetime.datetime.strptime(event["event_date"], "%Y-%m-%d %H:%M:%S"), 
+            event["event_type"], 
+            event["machine_name"], 
+            event["user"]
+            )
+          for event in events
+        ]
+    except FileNotFoundError:
+      print("Error: File not found.")
+      return []
 
-def generate_report(machines):
-  for machine, users in machines.items():
-    if len(users) > 0:
-      user_list = ", ".join(users)
-      print("{}: {}".format(machine, user_list))
+  # Takes a list of events and returns a distionary of machines and users that are logged in at the given access time
+  def current_users(self, events, start_time, end_time):
+    if not events:
+      return {}
+    
+    #start_time = datetime.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    #end_time = datetime.datetime.strptime(end_time, "%Y-%m-%d %H:%M:%S")
 
-events = [
-    Event('2020-01-21 12:45:56', 'login', 'myworkstation.local', 'jordan'),
-    Event('2020-01-22 15:53:42', 'logout', 'webserver.local', 'jordan'),
-    Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
-    Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
-    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
-    Event('2020-01-23 11:24:35', 'logout', 'mailserver.local', 'chris'),
-]
+    events.sort(key=lambda event: event.date)
+    #new_events = [event for event in events if (start_time <= event.date and event.date <= end_time)]
 
-users = current_users(events)
-print(users)
+    machines = {}
+    for event in events:
+      if event.machine not in machines:
+        machines[event.machine] = set()
+      if event.type == "login":
+        machines[event.machine].add(event.user)
+      elif event.type == "logout":
+        if event.user in machines[event.machine]:
+          machines[event.machine].remove(event.user)
+    
+    return machines
 
-generate_report(users)
+  # Track login time
+
+  def generate_report(self, machines, filename):
+    with open(filename, "w") as report:
+      for machine, users in machines.items():
+        if len(users) > 0:
+          user_list = ", ".join(users)
+          report.write("{}: {}\n".format(machine, user_list))
+
+
+def run_report():
+  report = UserAccessReport()
+  
+  events = report.read_log_file("events.csv")
+  users = report.current_users(events, "2024-01-01 00:00:00", "2024-12-31 23:59:59")
+  report.generate_report(users, "user_access_report.txt")
+
+if __name__ == "__main__":
+  run_report()
