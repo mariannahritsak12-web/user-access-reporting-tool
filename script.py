37477aa2001b560871d0140a0ce12245e68b167f
Marianna <mariannahritsak12@gmail.com>
2026-03-20 16:00:29 +0100

    Added login duration tracking functionality

--- script.py
+++ script.py
@@ -4,7 +4,6 @@
 from collections import defaultdict
 import csv
 import datetime
-import argparse
 import os
 
 # Represents a single login or logout event
@@ -60,6 +59,8 @@ class UserAccessReport:
         current_state[event.machine].add(event.user)
       elif event.type.lower() == "logout":
         current_state[event.machine].discard(event.user)
+      else:
+        print("Warning: Unknown event type")
 
       if event.date >= start_time:
         for user in current_state[event.machine]:
@@ -72,8 +73,21 @@ class UserAccessReport:
     login_time = None
     total_duration = datetime.timedelta()
 
+    relevant = [event for event in events if event.user == user and event.machine == machine]
 
-  def generate_report(self, machines, filename):
+    relevant.sort(key=lambda e : e.date)
+
+    for event in relevant:
+      if event.type.lower() == 'login':
+        login_time = event.date
+      elif event.type.lower() == 'logout' and login_time is not None:
+          duration = event.date - login_time
+          total_duration += duration
+          login_time = None
+
+    return total_duration
+
+  def generate_report(self, machines, events, filename):
     if len(machines) == 0:
       print("No machines with logged in users found.")
       return
@@ -84,10 +98,17 @@ class UserAccessReport:
     with open(filename, "w") as report:
       for machine, users in sorted(machines.items()):
         if len(users) > 0:
-          user_list = ", ".join(sorted(users))
-          report.write("{}: {}\n".format(machine, user_list))
+          report.write("MACHINE: {}\n".format(machine))
+          report.write("-"*20 + '\n')
+          for user in users:
+            total_duration = self.login_duration(events, user, machine)
+            duration_in_seconds = total_duration.total_seconds()
+            if duration_in_seconds > 0:
+              report.write(f"- {user} has been logged in for {round(duration_in_seconds, 3)}s\n")
+            else:
+              report.write(f"- {user} is still logged in\n")
+          report.write('\n')
     print("Report generated successfully: {}".format(filename))
-
 
 # Controls the flow of the program
 def run_report():
@@ -95,7 +116,7 @@ def run_report():
   
   events = report.read_log_file("events.csv")
   users = report.current_users(events, "2024-01-01 00:00:00", "2024-12-31 23:59:59")
-  report.generate_report(users, "user_access_report_test.txt")
+  report.generate_report(users, events, "user_access_report_test.txt")
 
 # Entry point of the program
 if __name__ == "__main__":
